7eefde4371163300560f83bb68da27ed40bfc162
BrendanMcGaw <46087405+BrendanMcGaw@users.noreply.github.com>
2020-06-20 15:48:28 +1000

    etch-a-sketch
    Progress towards making a user chosen grid-size.

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
 // Takes the div element already created in the HTML file and assigns it a varible.
 const gridContainer = document.getElementById("gridContainer");
-var gridSize = 64;
-function chooseGridSize() {
-    prompt
+var rows = document.getElementsByClassName("rows");
+var squares = document.getElementsByClassName("squares");
+
+var gridSize = 16;
+var rowHeight = 960 / gridSize;
+
+// height must be 960px / gridSize
+
+// Creates rows amounting to the size of the grid.
+for (r = 0; r < gridSize; r++) {
+    var newRow = document.createElement('div');
+    gridContainer.appendChild(newRow).className = "rows";
 }
-// our iterator.
+
+
 
 // Creates 16 iterations of new square divs to create my grid.
 for (i = 0; i < gridSize; i++){
-    // Creates a new div element and assigns it ot the newSquare variable.
-    var newSquare = document.createElement('div');
-    newSquare.className = "square";
-
-    // Appends means placing it inside of the other div or making it the parent node.
-    gridContainer.appendChild(newSquare);
+    for (cell = 0; cell < rows.length; cell++) {
+        var newSquare = document.createElement('div');
+        newSquare.className = "squares";
+        rows[i].appendChild(newSquare);
+    }
 }
-var squares = document.getElementsByClassName("square");
+
 
 // looks for the index number of the square the you have moused over and applies the style
 // to that particular square
